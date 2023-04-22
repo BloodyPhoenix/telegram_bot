bfb3bc5112dc42918b6761d82021ae75208130be
Maria Chashchina <mchashchina@gmail.com>
2023-04-22 18:56:45 +0300

    Made tests for weather button

--- tests/test_handlers/test_keyboard_handlers/test_menu_handler.py
+++ tests/test_handlers/test_keyboard_handlers/test_menu_handler.py
@@ -4,9 +4,9 @@ from bot.commands.buttons_handlers import show_menu
 from bot.commands.keyboards import MENU_KEYBOARD
 
 
+
 @pytest.mark.asyncio
 async def test_start_handler():
     message = AsyncMock()
-    state = AsyncMock()
-    await show_menu(message, state)
+    await show_menu(message)
     message.answer.assert_called_with(text="Меню", reply_markup=MENU_KEYBOARD)
---
+++ tests/test_handlers/test_keyboard_handlers/test_weather_handler.py
@@ -0,0 +1,31 @@
+import pytest
+from unittest.mock import AsyncMock
+from bot.commands.buttons_handlers import town_name_input, town_weather
+from bot.commands.keyboards import MENU_KEYBOARD
+from bot.commands.states import TownInputStates
+
+
+@pytest.mark.asyncio
+async def test_start_handler():
+    message = AsyncMock()
+    state = AsyncMock()
+    await town_name_input(message, state)
+    message.answer.assert_called_with(text="Введите название города", reply_markup=MENU_KEYBOARD)
+    state.set_state.assert_called_with(TownInputStates.town_name_input)
+
+
+async def test_false_town_name():
+    message = AsyncMock(text="Абырвалг")
+    await town_weather(message)
+    message.answer.assert_called_with(
+        text="К сожалению, запрошенный вами населённый пункт или область найти не удалось. "
+             "Пожалуйста, проверьте, не была ли допущена опечатка."
+    )
+
+
+@pytest.mark.asyncio
+async def test_correct_town_name():
+    message = AsyncMock(text="Москва")
+    state = AsyncMock()
+    await town_weather(message)
+    float(message.answer.text)
